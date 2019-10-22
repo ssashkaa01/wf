89058e3284d357a6265ac5abceb3a6dc8179c0eb
STORYM <ssashkaa01@gmail.com>
2019-10-22 13:38:57 +0300

    add hw

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ми і не сумнівалися, що ви так думаєте!");
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Старайтесь краще)");
+        }
+
+        private void buttonNo_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Random r = new Random();
+            buttonNo.Left = r.Next(0, this.Size.Width - buttonNo.Width);
+            buttonNo.Top = r.Next(0, this.Size.Height - buttonNo.Height);
+        }
+    }
+}
