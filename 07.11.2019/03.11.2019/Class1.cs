87042e4b4f3e5b005088fadd34c5574eef09a339
STORYM <ssashkaa01@gmail.com>
2019-11-07 22:46:58 +0200

    add hw

---
+++ 07.11.2019/03.11.2019/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._11._2019
+{
+    [Serializable]
+    class Order
+    {
+        private List<string> goods;
+        private int sum;
+        private int ID { get; set; }
+        private DateTime time { get; set; }
+
+        public Order(int id)
+        {
+            goods = new List<string>();
+            ID = id;
+            time = DateTime.Now;
+        }
+
+        public void Add(string good, int sumGood)
+        {
+            goods.Add(good);
+            sum += sumGood;
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+
+        public string GetOrder()
+        {
+            string order = "";
+
+            foreach (string good in goods)
+            {
+                order += $"{good}\n";
+            }
+
+            return order;
+        }
+
+        public DateTime GetTime()
+        {
+            return time;
+        }
+
+        public string GetDescription()
+        {
+            return $"#{GetID()} :: {GetTime()}";
+        }
+        public int GetSum()
+        {
+            return sum;
+        }
+    }
+}
---
+++ 07.11.2019/03.11.2019/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace _03._11._2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupDrinks = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.buttonOrder = new System.Windows.Forms.Button();
+            this.groupMainCourse = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.groupDeserts = new System.Windows.Forms.GroupBox();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton12 = new System.Windows.Forms.RadioButton();
+            this.radioButton13 = new System.Windows.Forms.RadioButton();
+            this.radioButton14 = new System.Windows.Forms.RadioButton();
+            this.radioButton15 = new System.Windows.Forms.RadioButton();
+            this.listOrder = new System.Windows.Forms.ListBox();
+            this.buttonDetails = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupDrinks.SuspendLayout();
+            this.groupMainCourse.SuspendLayout();
+            this.groupDeserts.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupDrinks
+            // 
+            this.groupDrinks.Controls.Add(this.radioButton5);
+            this.groupDrinks.Controls.Add(this.radioButton4);
+            this.groupDrinks.Controls.Add(this.radioButton3);
+            this.groupDrinks.Controls.Add(this.radioButton2);
+            this.groupDrinks.Controls.Add(this.radioButton1);
+            this.groupDrinks.Location = new System.Drawing.Point(12, 12);
+            this.groupDrinks.Name = "groupDrinks";
+            this.groupDrinks.Size = new System.Drawing.Size(221, 279);
+            this.groupDrinks.TabIndex = 1;
+            this.groupDrinks.TabStop = false;
+            this.groupDrinks.Text = "Drinks";
+            this.toolTip1.SetToolTip(this.groupDrinks, "vodka is alcogol");
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(24, 227);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(139, 24);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Beer (60 UAH)";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(24, 177);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(150, 24);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Wine (200 UAH)";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 129);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(160, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Vodka (120 UAH)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 81);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(132, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tea (10 UAH)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 37);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(153, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Coffee (20 UAH)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // buttonOrder
+            // 
+            this.buttonOrder.Location = new System.Drawing.Point(12, 305);
+            this.buttonOrder.Name = "buttonOrder";
+            this.buttonOrder.Size = new System.Drawing.Size(221, 48);
+            this.buttonOrder.TabIndex = 2;
+            this.buttonOrder.Text = "ORDER";
+            this.buttonOrder.UseVisualStyleBackColor = true;
+            this.buttonOrder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupMainCourse
+            // 
+            this.groupMainCourse.Controls.Add(this.radioButton6);
+            this.groupMainCourse.Controls.Add(this.radioButton7);
+            this.groupMainCourse.Controls.Add(this.radioButton8);
+            this.groupMainCourse.Controls.Add(this.radioButton9);
+            this.groupMainCourse.Controls.Add(this.radioButton10);
+            this.groupMainCourse.Location = new System.Drawing.Point(266, 12);
+            this.groupMainCourse.Name = "groupMainCourse";
+            this.groupMainCourse.Size = new System.Drawing.Size(299, 279);
+            this.groupMainCourse.TabIndex = 5;
+            this.groupMainCourse.TabStop = false;
+            this.groupMainCourse.Text = "Main Course";
+            this.toolTip2.SetToolTip(this.groupMainCourse, "Vega cuttlets bad");
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(24, 227);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(143, 24);
+            this.radioButton6.TabIndex = 4;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Soup (40 UAH)";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(24, 177);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(234, 24);
+            this.radioButton7.TabIndex = 3;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Vegetarian cutlets (55 UAH)";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(24, 129);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(143, 24);
+            this.radioButton8.TabIndex = 2;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Pizza (80 UAH)";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(24, 81);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(218, 24);
+            this.radioButton9.TabIndex = 1;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Baked potatoes (60 UAH)";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Checked = true;
+            this.radioButton10.Location = new System.Drawing.Point(24, 37);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(189, 24);
+            this.radioButton10.TabIndex = 0;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Pork steak (230 UAH)";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // groupDeserts
+            // 
+            this.groupDeserts.Controls.Add(this.radioButton11);
+            this.groupDeserts.Controls.Add(this.radioButton12);
+            this.groupDeserts.Controls.Add(this.radioButton13);
+            this.groupDeserts.Controls.Add(this.radioButton14);
+            this.groupDeserts.Controls.Add(this.radioButton15);
+            this.groupDeserts.Location = new System.Drawing.Point(591, 12);
+            this.groupDeserts.Name = "groupDeserts";
+            this.groupDeserts.Size = new System.Drawing.Size(272, 279);
+            this.groupDeserts.TabIndex = 5;
+            this.groupDeserts.TabStop = false;
+            this.groupDeserts.Text = "Deserts";
+            this.toolTip3.SetToolTip(this.groupDeserts, "Jelly as plasticine");
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(24, 227);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(181, 24);
+            this.radioButton11.TabIndex = 4;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Smoothies (41 UAH)";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            this.radioButton12.AutoSize = true;
+            this.radioButton12.Location = new System.Drawing.Point(24, 177);
+            this.radioButton12.Name = "radioButton12";
+            this.radioButton12.Size = new System.Drawing.Size(164, 24);
+            this.radioButton12.TabIndex = 3;
+            this.radioButton12.TabStop = true;
+            this.radioButton12.Text = "Tiramisu (56 UAH)";
+            this.radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            this.radioButton13.AutoSize = true;
+            this.radioButton13.Location = new System.Drawing.Point(24, 129);
+            this.radioButton13.Name = "radioButton13";
+            this.radioButton13.Size = new System.Drawing.Size(135, 24);
+            this.radioButton13.TabIndex = 2;
+            this.radioButton13.TabStop = true;
+            this.radioButton13.Text = "Jelly (22 UAH)";
+            this.radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            this.radioButton14.AutoSize = true;
+            this.radioButton14.Location = new System.Drawing.Point(24, 81);
+            this.radioButton14.Name = "radioButton14";
+            this.radioButton14.Size = new System.Drawing.Size(175, 24);
+            this.radioButton14.TabIndex = 1;
+            this.radioButton14.TabStop = true;
+            this.radioButton14.Text = "Pancakes (38 UAH)";
+            this.radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton15
+            // 
+            this.radioButton15.AutoSize = true;
+            this.radioButton15.Checked = true;
+            this.radioButton15.Location = new System.Drawing.Point(24, 37);
+            this.radioButton15.Name = "radioButton15";
+            this.radioButton15.Size = new System.Drawing.Size(202, 24);
+            this.radioButton15.TabIndex = 0;
+            this.radioButton15.TabStop = true;
+            this.radioButton15.Text = "Cheesecakes (45 UAH)";
+            this.radioButton15.UseVisualStyleBackColor = true;
+            // 
+            // listOrder
+            // 
+            this.listOrder.FormattingEnabled = true;
+            this.listOrder.ItemHeight = 20;
+            this.listOrder.Location = new System.Drawing.Point(266, 305);
+            this.listOrder.Name = "listOrder";
+            this.listOrder.Size = new System.Drawing.Size(597, 264);
+            this.listOrder.TabIndex = 6;
+            // 
+            // buttonDetails
+            // 
+            this.buttonDetails.Location = new System.Drawing.Point(12, 359);
+            this.buttonDetails.Name = "buttonDetails";
+            this.buttonDetails.Size = new System.Drawing.Size(221, 48);
+            this.buttonDetails.TabIndex = 7;
+            this.buttonDetails.Text = "DETAILS";
+            this.buttonDetails.UseVisualStyleBackColor = true;
+            this.buttonDetails.Click += new System.EventHandler(this.buttonDetails_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(12, 413);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(221, 48);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 467);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(221, 48);
+            this.buttonSave.TabIndex = 9;
+            this.buttonSave.Text = "SAVE";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 521);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(221, 48);
+            this.buttonLoad.TabIndex = 10;
+            this.buttonLoad.Text = "LOAD";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 578);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonDetails);
+            this.Controls.Add(this.listOrder);
+            this.Controls.Add(this.groupDeserts);
+            this.Controls.Add(this.groupMainCourse);
+            this.Controls.Add(this.buttonOrder);
+            this.Controls.Add(this.groupDrinks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupDrinks.ResumeLayout(false);
+            this.groupDrinks.PerformLayout();
+            this.groupMainCourse.ResumeLayout(false);
+            this.groupMainCourse.PerformLayout();
+            this.groupDeserts.ResumeLayout(false);
+            this.groupDeserts.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupDrinks;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button buttonOrder;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupMainCourse;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.GroupBox groupDeserts;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton12;
+        private System.Windows.Forms.RadioButton radioButton13;
+        private System.Windows.Forms.RadioButton radioButton14;
+        private System.Windows.Forms.RadioButton radioButton15;
+        private System.Windows.Forms.ListBox listOrder;
+        private System.Windows.Forms.Button buttonDetails;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+    }
+}
+
